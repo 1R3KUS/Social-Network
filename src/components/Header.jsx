93ace00a63880a38df56e319e50b5e4994923b6a
1R3KUS <r3kus05@gmail.com>
2020-06-19 15:50:20 +0300

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+  return (<header className='header'>
+    <img src='https://upload.wikimedia.org/wikipedia/commons/a/ac/Oikya_Front_Logo.png' alt='Something' />
+  </header>)
+}
+
+export default Header;
