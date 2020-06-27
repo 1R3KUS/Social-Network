de01aa372171a539cbb72d0c7357c261c3ee80b7
1R3KUS <r3kus05@gmail.com>
2020-06-27 12:45:49 +0300

    Урок 21 сверстал страницу Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,32 @@ import s from'./Dialogs.module.css';
 
 function Dialogs(props) {
   return (
-    <div className={s.content}>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>
+          Abu
+        </div>
+        <div className={s.dialog}>
+          Abdulla
+        </div>
+        <div className={s.dialog}>
+          Adam
+        </div>
+        <div className={s.dialog}>
+          Seyf
+        </div>
+        <div className={s.dialog}>
+          Halid
+        </div>
+        <div className={s.dialog}>
+          Shamil
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How are you?</div>
+      </div>
     </div>
   )
 }
